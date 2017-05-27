51af59fedf80a6bfce9c73c68e767e052140b874
ssnitish <ssnitish0000@gmail.com>
2017-05-27 23:39:13 +0530

    Create Solution.java
    Implement stack, and do, 1- Push, 2- Pop, 3-Maximum in stack at anytime.

---
+++ Stacks/Maximum Elements/Solution.java
@@ -0,0 +1,39 @@
+//implement stack and query , 1-push ,2-pop, 3-max in stack
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // no. of queries
+        Stack<Integer> maxStack = new Stack<Integer>(); // to store max element 
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int max = 0;
+        for(int i=0; i< n; i++){
+            int query = sc.nextInt();
+            switch(query){
+                case 1:
+                    int x = sc.nextInt();
+                    stack.push(x);
+                    if (maxStack.isEmpty() || x >= maxStack.peek()) {
+                            maxStack.push(x);
+                    }
+                    break;
+                case 2:
+                    int poppedValue = stack.pop();
+                    if(poppedValue == maxStack.peek())
+                        maxStack.pop();
+                    break;
+                case 3:
+                    System.out.println(maxStack.peek());
+                    break;
+                
+                
+            }
+            
+        }
+    }
+}
