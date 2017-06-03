6497dbe5f067bfc5e091abf1b561c28530ec8dbc
ssnitish <ssnitish0000@gmail.com>
2017-06-03 23:21:29 +0530

    Create Solution.java

---
+++ Linked Lists/Cycle Detection/Solution.java
@@ -0,0 +1,25 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    if(head == null) return false;
+   
+    Node slow = head;
+    Node fast = head.next;    
+    while(slow != null && fast != null && fast.next != null){
+        if(fast == slow){
+            return true;
+        }
+        fast = fast.next.next;
+        slow = slow.next;            
+    }    
+    return false;
+    
+}
